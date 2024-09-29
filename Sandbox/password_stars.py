1ff061fbd1cdd6969e35e1674d8b87b36fa4b60f
shutong chen <shutong.chen@my.jcu.edu.au>
2024-09-30 00:55:59 +0800

    Add File to Git

--- Sandbox/password_stars.py
+++
@@ -1,7 +0,0 @@
-LENGTH_STANDARD = 6
-word = input("Enter your word: ")
-while len(word) < LENGTH_STANDARD:
-    print("Invalid")
-    word = input("Enter your word: ")
-for i in word:
-    print("*", end="")
